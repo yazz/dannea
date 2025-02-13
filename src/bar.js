449f14ee1d479d30838369301aae841405d6aa19
yazz <zubairq@gmail.com>
2025-02-13 13:56:00 +0100

    trying to connect to SUI wallet

--- src/bar.js
+++ src/bar.js
@@ -1,6 +1,8 @@
+import { createNetworkConfig, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+
+
 export default function bar() {
-    //
-
-    alert("Hallo ducks2")
-
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import bar from './bar.js';
-debugger
+import { createNetworkConfig, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+//debugger
 bar();
+
+// Config options for the networks you want to connect to
+const { networkConfig } = createNetworkConfig({
+    localnet: { url: getFullnodeUrl('localnet') },
+    mainnet: { url: getFullnodeUrl('mainnet') },
+});
+const queryClient = new QueryClient();
